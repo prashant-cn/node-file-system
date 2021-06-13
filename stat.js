12462ead81574e949673944bd4ca8b8134b62a57
Prashant Kumar <prashant_hem@yahoo.co.in>
2021-06-13 16:39:11 +0530

    Stat|File Systems

--- stat.js
+++ stat.js
@@ -1,2 +1,26 @@
+//fs.stat(file/path, callback) -> asynchronous
+//fs.statSync(file/path) -> synchronous
+
 const fs = require('fs')
 
+//file path
+const textFile = "files/textFile.txt"
+
+//synchronous
+let stats = fs.statSync(textFile)
+console.log(stats)
+//we have multiple "is" option with stats too
+console.log(stats.isDirectory()) //false
+console.log(stats.isFile()) //true
+console.log('synchronous')
+
+
+//asynchronous
+
+fs.stat(textFile, (error, stats)=> {
+    if(error){
+        throw error
+    }
+    console.log(stats)
+})
+console.log('asynchronous')
