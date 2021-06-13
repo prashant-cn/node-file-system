c7d8ae67d9354116cfc73f33995be1b4b19e5089
Prashant Kumar <prashant_hem@yahoo.co.in>
2021-06-13 20:22:12 +0530

    readFile & readFileSync|File Systems

---
+++ readFile.js
@@ -0,0 +1,18 @@
+//fs.readFile(file, {encoding, flag}, callback) -> asynchronous
+//fs.readFileSync(file, {encoding, flag}) -> synchronous
+const fs = require('fs')
+
+//file path
+const textFile = "files/textFile.txt"
+
+//sync
+let data = fs.readFileSync(textFile, {encoding:'utf8'})
+console.log(data) //use try-catch block if error handling needed in sync
+
+//async
+fs.readFile(textFile, {encoding: 'utf8'}, (error, data)=> {
+    if(error){
+        console.log(error);
+    }
+    console.log(data)
+})
