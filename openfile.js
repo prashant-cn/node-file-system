05c2bd3c4394e8c18587ec97a272941a207ed70f
Prashant Kumar <prashant_hem@yahoo.co.in>
2021-06-13 16:22:27 +0530

    Open File|File Systems

---
+++ openfile.js
@@ -0,0 +1,20 @@
+//fs.open(file, flag, [mode], callback) -> asynchronous
+//fs.openSync(file, flag, [mode], callback) -> synchronous
+
+const fs = require('fs')
+
+//file path
+const textFile = "files/textFile.txt"
+
+fs.open(textFile, 'w+', (error, filedescriptor)=> {
+    if(error){
+        return console.log(`Error in reading file: ${error}`)
+    }
+    //perform any operation on file: read write append etc
+    console.log(`File ${filedescriptor} opened succesfully.`);
+
+    //close the file
+    fs.close(filedescriptor, (error)=> {
+        console.log(`File closed.`);
+    })
+})
